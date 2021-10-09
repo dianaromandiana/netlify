21a45a9cafa2fd9fd305727c9568c3778cfedf36
dianalaput <dianaroman@mail.com>
2021-10-09 16:04:16 +0300

    pull request

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// import another component
-import main from './main';
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path ='/index.html';
 
-main();
+//console.log(`__dirname  ${__dirname}`);
+//console.log(`__filename  ${__filename}`);
+
+app.use(express.static(__dirname + "/netlifySDF"));
+app.use("/", function(request, response){
+    response.send("<h1>Главная страница</h1>");
+});
+app.listen(PORT);
+console.log(`приложение запущено по порту ${PORT}`);
